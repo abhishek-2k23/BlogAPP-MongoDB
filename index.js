3a8a03d790a3d058c753db682477104a3d651e65
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-09-21 23:20:33 +0530

    app export

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import fileupload from "express-fileupload";
 import cookieParser from "cookie-parser";
 import { cloudinaryConnect } from "./Configuration/cloudinary.js";
 
+
 // app intialization
 const app = express();
 
@@ -20,13 +21,26 @@ app.use(fileupload({
 }));
 app.use(cookieParser());
 
+//for serverless 
+app.use((req, res,error, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+
+    next();
+})
 //configurations
 dbConnection();
 cloudinaryConnect();
 
 //server up
-app.listen(process.env.DB_PORT,()=>{console.log("server is up at ",process.env.DB_PORT)});
-app.get("/",(req,res)=>{res.send(`<h1>Welcome to the Home Route of the Blog App Backend`)});
+app.get((req,res)=>{res.send(`<h1>Welcome to the Home Route of the Blog App Backend`)});
+
+
 
 //Route Mounting
 import postRouter from "./Routes/post.js";
@@ -35,3 +49,10 @@ import userRouter from "./Routes/user.js";
 //mounting routes
 app.use("/post",postRouter);
 app.use("/auth",userRouter);
+
+
+
+//exporting our app on serverless
+
+
+export {app};
