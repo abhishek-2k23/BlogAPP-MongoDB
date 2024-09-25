cbed16179a0c60817bc48a3f1ceb5e413196455e
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-09-25 23:30:58 +0530

    aws-lambda mongoose connection

--- Configuration/db.js
+++ Configuration/db.js
@@ -1,16 +1,25 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const dbConnection = () =>{
+let conn = null;
 
-        mongoose.connect(process.env.DB_URL,{
-            useNewUrlParser : true,
-            useUnifiedTopology : true,
-        }).then(() => {console.log("DB connection Done.")})
-        .catch(() => console.log("Error in connection of DB"))
-   
+const dbConnection = async() => {
+    if(conn == null){
+        console.log("creating new connection");
+        conn =  mongoose.connect(process.env.DB_URL).then(() => mongoose);
+        await conn;
+
+        // const postdata = await posts.find();
+        // console.log('connection established');
+
+        // await mongoose.disconnect();
+        // return postdata;
+        return conn;
+    }
+
+    console.log('connection already established');
+    return conn;
 }
 
 export default dbConnection;
