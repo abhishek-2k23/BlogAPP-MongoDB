6cecf029816dc39632f82fcbf108b6abf3206f46
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-09-25 23:30:33 +0530

    aws handler

--- serverless.js
+++ serverless.js
@@ -1,5 +1,13 @@
 import serverless from 'serverless-http';
 import {app} from './index.js';
-
-const AppHandler = serverless(app);
-export  {AppHandler};
+import dbConnection from './Configuration/db.js';
+const AppHandler = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+  
+    const connection = await dbConnection();
+    const response = await serverless(app)(event, context);
+    await connection.disconnect();
+  
+    return response;
+  };;
+export {AppHandler};
